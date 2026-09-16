8bb1a8ad05138e09c75a8333ebffb494f06b4a4d
Rajeev Upadhyay <rajeev.upadhyay@live.in>
2026-09-16 16:06:40 +0530

    feat: add Vercel serverless entry point api/index.ts

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+﻿import { handle } from 'hono/vercel'
+import { AlbumController, ArtistController, SearchController, SongController } from '../src/modules/index'
+import { PlaylistController } from '../src/modules/playlists/controllers/index'
+import { App } from '../src/app'
+
+const app = new App([
+  new SearchController(),
+  new SongController(),
+  new AlbumController(),
+  new ArtistController(),
+  new PlaylistController()
+]).getApp()
+
+export default handle(app)
